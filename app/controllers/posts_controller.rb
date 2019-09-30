78046e7e73a42adc94ca3bf7efd35d0bbf858ab8
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-30 03:19:58 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,11 @@ class PostsController < ApplicationController
 
 
   def create
-    @post = Post.new
-    @post.title = params[:title]
-    @post.description = params[:description]
-    @post.save
-    redirect_to post_path(@post)
-  end
+  @post = Post.new
+  @post.title = params[:title]
+  @post.description = params[:description]
+  @post.save
+  redirect_to post_path(@post)
+end
   # add create method here
 end
